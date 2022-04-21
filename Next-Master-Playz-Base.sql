d4907ca9e1df031c4d6f4d1e2ae1d85294daf9cf
NEXT MASTER PLAYZ <74363325+Nextmasterplayz@users.noreply.github.com>
2022-04-21 19:03:04 +0530

    Add files via upload

---
+++ Next-Master-Playz-Base.sql
@@ -0,0 +1,374 @@
+
+
+-- Dumping database structure for nmp
+CREATE DATABASE IF NOT EXISTS `Next-Master-Playz-Base` /*!40100 DEFAULT CHARACTER SET utf8mb4 */;
+USE `Next-Master-Playz-Base`;
+
+-- Dumping structure for table nmp.addon_account
+CREATE TABLE IF NOT EXISTS `addon_account` (
+  `name` varchar(60) NOT NULL,
+  `label` varchar(100) NOT NULL,
+  `shared` int(11) NOT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Dumping data for table nmp.addon_account: ~1 rows (approximately)
+/*!40000 ALTER TABLE `addon_account` DISABLE KEYS */;
+INSERT INTO `addon_account` (`name`, `label`, `shared`) VALUES
+	('society_cardealer', 'Concessionnaire', 1);
+/*!40000 ALTER TABLE `addon_account` ENABLE KEYS */;
+
+-- Dumping structure for table nmp.addon_account_data
+CREATE TABLE IF NOT EXISTS `addon_account_data` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `account_name` varchar(100) DEFAULT NULL,
+  `money` int(11) NOT NULL,
+  `owner` varchar(40) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `index_addon_account_data_account_name_owner` (`account_name`,`owner`),
+  KEY `index_addon_account_data_account_name` (`account_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Dumping data for table nmp.addon_account_data: ~0 rows (approximately)
+/*!40000 ALTER TABLE `addon_account_data` DISABLE KEYS */;
+/*!40000 ALTER TABLE `addon_account_data` ENABLE KEYS */;
+
+-- Dumping structure for table nmp.addon_inventory
+CREATE TABLE IF NOT EXISTS `addon_inventory` (
+  `name` varchar(60) NOT NULL,
+  `label` varchar(100) NOT NULL,
+  `shared` int(11) NOT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Dumping data for table nmp.addon_inventory: ~1 rows (approximately)
+/*!40000 ALTER TABLE `addon_inventory` DISABLE KEYS */;
+INSERT INTO `addon_inventory` (`name`, `label`, `shared`) VALUES
+	('society_cardealer', 'Concesionnaire', 1);
+/*!40000 ALTER TABLE `addon_inventory` ENABLE KEYS */;
+
+-- Dumping structure for table nmp.addon_inventory_items
+CREATE TABLE IF NOT EXISTS `addon_inventory_items` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `inventory_name` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `count` int(11) NOT NULL,
+  `owner` varchar(40) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `index_addon_inventory_items_inventory_name_name` (`inventory_name`,`name`),
+  KEY `index_addon_inventory_items_inventory_name_name_owner` (`inventory_name`,`name`,`owner`),
+  KEY `index_addon_inventory_inventory_name` (`inventory_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Dumping data for table nmp.addon_inventory_items: ~0 rows (approximately)
+/*!40000 ALTER TABLE `addon_inventory_items` DISABLE KEYS */;
+/*!40000 ALTER TABLE `addon_inventory_items` ENABLE KEYS */;
+
+-- Dumping structure for table nmp.billing
+CREATE TABLE IF NOT EXISTS `billing` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `identifier` varchar(40) NOT NULL,
+  `sender` varchar(40) NOT NULL,
+  `target_type` varchar(50) NOT NULL,
+  `target` varchar(40) NOT NULL,
+  `label` varchar(255) NOT NULL,
+  `amount` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Dumping data for table nmp.billing: ~0 rows (approximately)
+/*!40000 ALTER TABLE `billing` DISABLE KEYS */;
+/*!40000 ALTER TABLE `billing` ENABLE KEYS */;
+
+-- Dumping structure for table nmp.cardealer_vehicles
+CREATE TABLE IF NOT EXISTS `cardealer_vehicles` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `vehicle` varchar(255) NOT NULL,
+  `price` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Dumping data for table nmp.cardealer_vehicles: ~0 rows (approximately)
+/*!40000 ALTER TABLE `cardealer_vehicles` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cardealer_vehicles` ENABLE KEYS */;
+
+-- Dumping structure for table nmp.datastore
+CREATE TABLE IF NOT EXISTS `datastore` (
+  `name` varchar(60) NOT NULL,
+  `label` varchar(100) NOT NULL,
+  `shared` int(11) NOT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Dumping data for table nmp.datastore: ~0 rows (approximately)
+/*!40000 ALTER TABLE `datastore` DISABLE KEYS */;
+/*!40000 ALTER TABLE `datastore` ENABLE KEYS */;
+
+-- Dumping structure for table nmp.datastore_data
+CREATE TABLE IF NOT EXISTS `datastore_data` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(60) NOT NULL,
+  `owner` varchar(40) DEFAULT NULL,
+  `data` longtext DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `index_datastore_data_name_owner` (`name`,`owner`),
+  KEY `index_datastore_data_name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Dumping data for table nmp.datastore_data: ~0 rows (approximately)
+/*!40000 ALTER TABLE `datastore_data` DISABLE KEYS */;
+/*!40000 ALTER TABLE `datastore_data` ENABLE KEYS */;
+
+-- Dumping structure for table nmp.glovebox_inventory
+CREATE TABLE IF NOT EXISTS `glovebox_inventory` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `plate` varchar(8) NOT NULL,
+  `data` text NOT NULL,
+  `owned` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `plate` (`plate`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Dumping data for table nmp.glovebox_inventory: ~0 rows (approximately)
+/*!40000 ALTER TABLE `glovebox_inventory` DISABLE KEYS */;
+/*!40000 ALTER TABLE `glovebox_inventory` ENABLE KEYS */;
+
+-- Dumping structure for table nmp.items
+CREATE TABLE IF NOT EXISTS `items` (
+  `name` varchar(50) NOT NULL,
+  `label` varchar(50) NOT NULL,
+  `limit` int(11) NOT NULL DEFAULT -1,
+  `rare` int(11) NOT NULL DEFAULT 0,
+  `can_remove` int(11) NOT NULL DEFAULT 1,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Dumping data for table nmp.items: ~1 rows (approximately)
+/*!40000 ALTER TABLE `items` DISABLE KEYS */;
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
+	('contract', 'Contract', 1, 0, 1);
+/*!40000 ALTER TABLE `items` ENABLE KEYS */;
+
+-- Dumping structure for table nmp.jobs
+CREATE TABLE IF NOT EXISTS `jobs` (
+  `name` varchar(50) NOT NULL,
+  `label` varchar(50) DEFAULT NULL,
+  `whitelisted` tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Dumping data for table nmp.jobs: ~1 rows (approximately)
+/*!40000 ALTER TABLE `jobs` DISABLE KEYS */;
+INSERT INTO `jobs` (`name`, `label`, `whitelisted`) VALUES
+	('cardealer', 'Concessionnaire', 0);
+/*!40000 ALTER TABLE `jobs` ENABLE KEYS */;
+
+-- Dumping structure for table nmp.job_grades
+CREATE TABLE IF NOT EXISTS `job_grades` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `job_name` varchar(50) DEFAULT NULL,
+  `grade` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `label` varchar(50) NOT NULL,
+  `salary` int(11) NOT NULL,
+  `skin_male` longtext NOT NULL,
+  `skin_female` longtext NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=129 DEFAULT CHARSET=utf8mb4;
+
+-- Dumping data for table nmp.job_grades: ~4 rows (approximately)
+/*!40000 ALTER TABLE `job_grades` DISABLE KEYS */;
+INSERT INTO `job_grades` (`id`, `job_name`, `grade`, `name`, `label`, `salary`, `skin_male`, `skin_female`) VALUES
+	(1, 'cardealer', 0, 'recruit', 'Recrue', 10, '{}', '{}'),
+	(2, 'cardealer', 1, 'novice', 'Novice', 25, '{}', '{}'),
+	(3, 'cardealer', 2, 'experienced', 'Experimente', 40, '{}', '{}'),
+	(4, 'cardealer', 3, 'boss', 'Patron', 0, '{}', '{}');
+/*!40000 ALTER TABLE `job_grades` ENABLE KEYS */;
+
+-- Dumping structure for table nmp.licenses
+CREATE TABLE IF NOT EXISTS `licenses` (
+  `type` varchar(60) NOT NULL,
+  `label` varchar(60) NOT NULL,
+  PRIMARY KEY (`type`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Dumping data for table nmp.licenses: ~0 rows (approximately)
+/*!40000 ALTER TABLE `licenses` DISABLE KEYS */;
+/*!40000 ALTER TABLE `licenses` ENABLE KEYS */;
+
+-- Dumping structure for table nmp.okokbilling
+CREATE TABLE IF NOT EXISTS `okokbilling` (
+  `id` int(50) NOT NULL AUTO_INCREMENT,
+  `receiver_identifier` varchar(255) NOT NULL,
+  `receiver_name` varchar(255) NOT NULL,
+  `author_identifier` varchar(255) NOT NULL,
+  `author_name` varchar(255) DEFAULT NULL,
+  `society` varchar(255) NOT NULL,
+  `society_name` varchar(255) NOT NULL,
+  `item` varchar(255) NOT NULL,
+  `invoice_value` int(50) NOT NULL,
+  `status` varchar(50) NOT NULL,
+  `notes` varchar(255) DEFAULT ' ',
+  `sent_date` varchar(255) NOT NULL,
+  `limit_pay_date` varchar(255) DEFAULT NULL,
+  `fees_amount` int(50) DEFAULT 0,
+  `paid_date` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Dumping data for table nmp.okokbilling: ~0 rows (approximately)
+/*!40000 ALTER TABLE `okokbilling` DISABLE KEYS */;
+/*!40000 ALTER TABLE `okokbilling` ENABLE KEYS */;
+
+-- Dumping structure for table nmp.owned_vehicles
+CREATE TABLE IF NOT EXISTS `owned_vehicles` (
+  `owner` varchar(40) NOT NULL,
+  `state` tinyint(1) NOT NULL DEFAULT 0,
+  `plate` varchar(12) NOT NULL,
+  `vehicle` longtext DEFAULT NULL,
+  `type` varchar(20) NOT NULL DEFAULT 'car',
+  `job` varchar(20) DEFAULT NULL,
+  `stored` tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`plate`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Dumping data for table nmp.owned_vehicles: ~0 rows (approximately)
+/*!40000 ALTER TABLE `owned_vehicles` DISABLE KEYS */;
+/*!40000 ALTER TABLE `owned_vehicles` ENABLE KEYS */;
+
+-- Dumping structure for table nmp.rented_vehicles
+CREATE TABLE IF NOT EXISTS `rented_vehicles` (
+  `vehicle` varchar(60) NOT NULL,
+  `plate` varchar(12) NOT NULL,
+  `player_name` varchar(255) NOT NULL,
+  `base_price` int(11) NOT NULL,
+  `rent_price` int(11) NOT NULL,
+  `owner` varchar(22) NOT NULL,
+  PRIMARY KEY (`plate`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Dumping data for table nmp.rented_vehicles: ~0 rows (approximately)
+/*!40000 ALTER TABLE `rented_vehicles` DISABLE KEYS */;
+/*!40000 ALTER TABLE `rented_vehicles` ENABLE KEYS */;
+
+-- Dumping structure for table nmp.society_moneywash
+CREATE TABLE IF NOT EXISTS `society_moneywash` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `identifier` varchar(60) NOT NULL,
+  `society` varchar(60) NOT NULL,
+  `amount` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Dumping data for table nmp.society_moneywash: ~0 rows (approximately)
+/*!40000 ALTER TABLE `society_moneywash` DISABLE KEYS */;
+/*!40000 ALTER TABLE `society_moneywash` ENABLE KEYS */;
+
+-- Dumping structure for table nmp.trunk_inventory
+CREATE TABLE IF NOT EXISTS `trunk_inventory` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `plate` varchar(8) NOT NULL,
+  `data` text NOT NULL,
+  `owned` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `plate` (`plate`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Dumping data for table nmp.trunk_inventory: ~0 rows (approximately)
+/*!40000 ALTER TABLE `trunk_inventory` DISABLE KEYS */;
+/*!40000 ALTER TABLE `trunk_inventory` ENABLE KEYS */;
+
+-- Dumping structure for table nmp.users
+CREATE TABLE IF NOT EXISTS `users` (
+  `identifier` varchar(50) COLLATE utf8mb4_bin NOT NULL,
+  `license` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
+  `money` int(11) DEFAULT NULL,
+  `bank` int(11) DEFAULT NULL,
+  `permission_level` int(11) DEFAULT NULL,
+  `group` varchar(50) COLLATE utf8mb4_bin DEFAULT NULL,
+  `firstname` varchar(50) COLLATE utf8mb4_bin DEFAULT '',
+  `lastname` varchar(50) COLLATE utf8mb4_bin DEFAULT '',
+  `dateofbirth` varchar(25) COLLATE utf8mb4_bin DEFAULT '',
+  `sex` varchar(10) COLLATE utf8mb4_bin DEFAULT '',
+  `height` varchar(5) COLLATE utf8mb4_bin DEFAULT '',
+  `jail_time` int(11) NOT NULL DEFAULT 0,
+  `jail` int(11) NOT NULL DEFAULT 0,
+  `skin` longtext COLLATE utf8mb4_bin DEFAULT NULL,
+  PRIMARY KEY (`identifier`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+-- Dumping data for table nmp.users: ~1 rows (approximately)
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` (`identifier`, `license`, `money`, `bank`, `permission_level`, `group`, `firstname`, `lastname`, `dateofbirth`, `sex`, `height`, `jail_time`, `jail`, `skin`) VALUES
+	('steam:11000014792d565', 'license:c0652c8ae24b7765c19be9591c713e3bb73ab61e', 0, 0, 0, 'user', '', '', '', '', '', 0, 0, NULL);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+
+-- Dumping structure for table nmp.user_licenses
+CREATE TABLE IF NOT EXISTS `user_licenses` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `type` varchar(60) NOT NULL,
+  `owner` varchar(40) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Dumping data for table nmp.user_licenses: ~0 rows (approximately)
+/*!40000 ALTER TABLE `user_licenses` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_licenses` ENABLE KEYS */;
+
+-- Dumping structure for table nmp.vehicles
+CREATE TABLE IF NOT EXISTS `vehicles` (
+  `name` varchar(60) NOT NULL,
+  `model` varchar(60) NOT NULL,
+  `price` int(11) NOT NULL,
+  `category` varchar(60) DEFAULT NULL,
+  PRIMARY KEY (`model`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Dumping data for table nmp.vehicles: ~2 rows (approximately)
+/*!40000 ALTER TABLE `vehicles` DISABLE KEYS */;
+INSERT INTO `vehicles` (`name`, `model`, `price`, `category`) VALUES
+	('Akuma', 'AKUMA', 100, 'motorcycles'),
+	('Bullet', 'bullet', 200, 'super');
+/*!40000 ALTER TABLE `vehicles` ENABLE KEYS */;
+
+-- Dumping structure for table nmp.vehicle_categories
+CREATE TABLE IF NOT EXISTS `vehicle_categories` (
+  `name` varchar(60) NOT NULL,
+  `label` varchar(60) NOT NULL,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Dumping data for table nmp.vehicle_categories: ~11 rows (approximately)
+/*!40000 ALTER TABLE `vehicle_categories` DISABLE KEYS */;
+INSERT INTO `vehicle_categories` (`name`, `label`) VALUES
+	('compacts', 'Compacts'),
+	('coupes', 'Coupés'),
+	('motorcycles', 'Motos'),
+	('muscle', 'Muscle'),
+	('offroad', 'Off Road'),
+	('sedans', 'Sedans'),
+	('sports', 'Sports'),
+	('sportsclassics', 'Sports Classics'),
+	('super', 'Super'),
+	('suvs', 'SUVs'),
+	('vans', 'Vans');
+/*!40000 ALTER TABLE `vehicle_categories` ENABLE KEYS */;
+
+-- Dumping structure for table nmp.vehicle_sold
+CREATE TABLE IF NOT EXISTS `vehicle_sold` (
+  `client` varchar(50) NOT NULL,
+  `model` varchar(50) NOT NULL,
+  `plate` varchar(50) NOT NULL,
+  `soldby` varchar(50) NOT NULL,
+  `date` varchar(50) NOT NULL,
+  PRIMARY KEY (`plate`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Dumping data for table nmp.vehicle_sold: ~0 rows (approximately)
+/*!40000 ALTER TABLE `vehicle_sold` DISABLE KEYS */;
+/*!40000 ALTER TABLE `vehicle_sold` ENABLE KEYS */;
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
---
+++ ReadMe.md
@@ -0,0 +1,65 @@
+
+
+* IF YOU NEED HELP JOIN THIS  [DISCORD] SERVER (https://discord.gg/sVCKCv4p7g)
+
+> Next-Master-Playz-Base
+
+> For Your own and best servers contact NEXT MASTER PLAYZ#5601 in discord 
+
+> Supported Languages by me are  English & Tamil
+
+
+* Recommended Programs for Localhost:
+  * [Xampp](https://www.apachefriends.org/download.html)
+  * [HeidiSQL](https://www.heidisql.com/download.php)
+  * [Visual Studio Code](https://code.visualstudio.com/download) Or another Editing software such as:
+  * [Notepad++](https://notepad-plus-plus.org/downloads/)
+
+* Recommended Programs for Private or vps host:
+  * [HeidiSQL](https://www.heidisql.com/download.php)
+  * [Visual Studio Code](https://code.visualstudio.com/download) Or another Editing software such as:
+  * [Notepad++](https://notepad-plus-plus.org/downloads/)
+
+
+
+## Download & Installation
+
+### Download from the GitHub 
+https://github.com/Nextmasterplayz/Next-Master-FiveM-Base-ESX
+
+
+
+## Installation for localhost
+- 1 ) Extract the server into a place of your choosing.
+- 2 ) Install the database from the base folder called Next-Master-Playz-Base.sql
+- 3 ) Edit all information in server.cfg according to what you need (Tutorial on each line included)
+- 4 ) Once you have finished editing server.cfg, start FXServer.exe, It will create a "txadmin" 4 digit pin for you SAVE THAT PIN
+- 5 ) Load up your browser and go to your server ip and port 40120 (For this example i'll use my desktop/localhost): 127.0.0.1:40120
+- 6 ) Enter the PIN given to you by the server command prompt screen on step 3.
+- 7 ) Create your user and password(This will create the primary account for your administrative needs)
+- 8 ) Click Settings and edit everything to your liking/needs.
+- 9 ) Go to the "DASHBOARD" tab and click "START".
+- 10 ) If you did everything properly, your server is running, enjoy!
+
+# Legal & Information
+
+Base ESX Start-up Server by Next-Master-Playz
+
+I did NOT create any of these scripts, they are just some of the basic scripts needed to start an ESX Server properly. ALL Credit to the ESX Team and the people who designed and modified/fixed the code.
+
+
+* SSH OR HTTPS : https://github.com/Nextmasterplayz/Next-Master-FiveM-Base.git
+
+
+The only script in here that is not from the [ESX-Framework](https://github.com/ESX-Framework/) 
+
+
+
+* IF YOU NEED HELP JOIN THIS  [DISCORD] SERVER (https://discord.gg/sVCKCv4p7g)
+
+Next-Master-Playz-Base
+
+For Your own and best servers contact NEXT MASTER PLAYZ#5601 in discord 
+
+Supported Language are English / Tamil 
+
